7f64e7b5096bb5785bbd99660eb33a7f6dc5069e
ghibhs <gabrlz@outlook.com>
2025-09-11 08:24:56 -0300

    limpeza

--- godot_projects/test-v01/the_scene.gd
+++ godot_projects/test-v01/the_scene.gd
@@ -1,9 +1,1 @@
 extends Node2D
-
-@onready var bow_scene: PackedScene = preload("res://art/bow.tscn")
-
-func _ready():
-	var bow = bow_scene.instantiate()
-	add_child(bow)
-	bow.position = Vector2(20, 20)
-	
